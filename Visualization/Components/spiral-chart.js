f441b87be62a14e458df413d98b5bb326890f66e
isabelSoares <isabel.srsoares@gmail.com>
2020-11-28 11:49:13 +0000

    Spiral chart works !
    Co-authored-by: Daniel Pereira <DanielPereira890@users.noreply.github.com>
    Co-authored-by: Rodrigo Sousa <Sousa99@users.noreply.github.com>

--- Visualization/Components/spiral-chart.js
+++ Visualization/Components/spiral-chart.js
@@ -8,6 +8,8 @@ const PERIODS_AVAILABLE = [
 var spiral_chart_svg
 var selected_period_months = PERIODS_AVAILABLE[1]
 var spiral_color_scale
+var spiral_heatmap
+var dataset_spiral_chart
 
 function build_spiral_chart() {
     spiral_chart_svg = d3.select("svg#spiral_chart");
@@ -17,36 +19,26 @@ function build_spiral_chart() {
     const margin = { "top": 0, "bottom": 20, "left": 20, "right": 20 }
     const chartRadius = svg_height / 2 - margin.bottom;
 
-    var dataset_brands_year = dataset_brands.map((a) => ([parseInt(a['Year']), parseInt(a['# Models'])]));
-
-    var dataset_brands_filtered_year = {};
-    dataset_brands_year.forEach( function(datum) {
-        if (datum[0] in dataset_brands_filtered_year) dataset_brands_filtered_year[datum[0]] += datum[1];
-        else dataset_brands_filtered_year[datum[0]] = 0;
-    });
-
-    var dataset_spiral_chart = [];
-    for (let key in dataset_brands_filtered_year)
-        dataset_spiral_chart.push({"Year": key, "Models": dataset_brands_filtered_year[key]});
-
+    dataset_spiral_chart = treatDataset();
     addColorScale(d3.extent(dataset_spiral_chart, datum => datum["Models"]), d3.rgb(153, 204, 255), d3.rgb(0, 76, 153));
 
     //set the options for the sprial heatmap
-    let heatmap = spiralHeatmap()
+    spiral_heatmap = spiralHeatmap()
         .radius(chartRadius)
         .holeRadiusProportion(0)
-        .arcsPerCoil(4)
+        .arcsPerCoil(selected_period_months['div'])
         .coilPadding(0.05)
-        .arcLabel("month")
-        .coilLabel("year")
+        .arcLabel("Month")
+        .coilLabel("Year")
 
     const g = spiral_chart_svg.append("g")
+        .attr("id", "spiral_group")
         .attr("transform", "translate(" + 
             (svg_width / 2) + "," + 
             (svg_height / 2 - margin.bottom + margin.top) + ")");
 
     g.datum(dataset_spiral_chart)
-        .call(heatmap);
+        .call(spiral_heatmap);
 
     g.selectAll(".arc").selectAll("path")
         .style("fill", function (d) { return spiral_color_scale(d["Models"]); })
@@ -61,6 +53,52 @@ function build_spiral_chart() {
             .text("Number of Models")
 
     addPeriodSelection();
+}
+
+function treatDataset() {
+    var groupBy = selected_period_months['by'];
+
+    var filteredDataset = [];
+    if (selected_brands.length == 0) filteredDataset = dataset_models;
+    else filteredDataset = dataset_models.filter(elem => selected_brands.includes(elem['Brand']));
+
+    var finalDataset = []
+    for (var year = start_date.getFullYear(); year <= end_date.getFullYear(); year ++) {
+        var step;
+        if (groupBy == 'month') step = 1;
+        else if (groupBy == 'quarter') step = 3;
+        else if (groupBy == 'semester') step = 6;
+
+        for (var month = 0; month < 12; month += step)
+            finalDataset.push({'Date': new Date(year, month, 1), 'Models': 0});
+            // finalDataset.push({'Date': new Date(year, month, 1), 'Year': year, 'Month': month, 'Models': 0});
+    }
+    
+    filteredDataset.forEach(function(elem) {
+        var month = undefined;
+        var year = elem['Date'].getFullYear();
+        
+        if (groupBy == 'month') month = elem['Date'].getMonth();
+        else if (groupBy == 'quarter') month = Math.floor(elem['Date'].getMonth() / 3) * 3;
+        else if (groupBy == 'semester') month = Math.floor(elem['Date'].getMonth() / 6) * 6;
+        
+        var date = new Date(year, month, 1)
+        var item = finalDataset.find(elem2 => elem2['Date'].getTime() == date.getTime());
+        var push_elem = {'Date': date, 'Models': 1};
+
+        if (item == undefined) console.log("Something went wrong!")
+        else item['Models'] += 1;
+    })
+
+    var sortedFinalDataset = finalDataset.sort((a, b) => a['Date'] - b['Date'])
+
+    for (var i = sortedFinalDataset.length - 1; i >= 0; i--) {
+        if (sortedFinalDataset[i]['Models'] == 0) sortedFinalDataset.pop();
+        else break;
+    }
+
+    console.log(sortedFinalDataset);
+    return sortedFinalDataset;
 }
 
 function addColorScale(datum ,startColor, endColor) {
@@ -89,24 +127,34 @@ function addColorScale(datum ,startColor, endColor) {
     var scale_g = spiral_chart_svg.append("g");
 
     scale_g.append("rect")
-        .attr("x", svg_width - (svg_width / 10 + WIDTH / 2))
+        .attr("x", svg_width - (svg_width / 10 + WIDTH))
         .attr("y", svg_height * 2 / 5 - HEIGHT / 3)
         .attr("width", WIDTH)
         .attr("height", HEIGHT)
         .style("fill", "url(#linearGradient)");
 
     scale_g.append("text")
-        .attr("x",  svg_width - (svg_width / 20 + WIDTH + 15))
-        .attr("y", HEIGHT / 3 + 180)
+        .attr("class", "spiral_scale_number")
+        .attr("id", "bottom_spiral_scale")
+        .attr("x", svg_width - (svg_width / 10 + WIDTH / 2))
+        .attr("y", svg_height * 2 / 5 + HEIGHT * 2 / 3 + svg_height * 0.06)
+        .attr("text-anchor", "middle")
+        .attr("dominant-baseline", "middle")
         .text(datum[0]);
     scale_g.append("text")
-        .attr("x", svg_width - (svg_width / 20 + WIDTH + 20))
-        .attr("y", HEIGHT / 2 - 10)
+        .attr("class", "spiral_scale_number")
+        .attr("id", "top_spiral_scale")
+        .attr("x", svg_width - (svg_width / 10 + WIDTH / 2))
+        .attr("y", svg_height * 2 / 5 - HEIGHT / 3 - svg_height * 0.06)
+        .attr("text-anchor", "middle")
+        .attr("dominant-baseline", "middle")
         .text(datum[1]);
 }
 
 function addPeriodSelection() {
-    const LINE_WIDTH = 300;
+    var svg_width = parseInt(spiral_chart_svg.style("width").slice(0, -2));
+
+    const LINE_WIDTH = svg_width * 0.75;
     const step = LINE_WIDTH / (PERIODS_AVAILABLE.length - 1);
 
     var svg_width = parseInt(spiral_chart_svg.style("width").slice(0, -2));
@@ -151,8 +199,7 @@ function addPeriodSelection() {
 
 function prepare_event_period_selection() {
     var svg_width = parseInt(spiral_chart_svg.style("width").slice(0, -2));
-    var svg_height = parseInt(spiral_chart_svg.style("height").slice(0, -2));
-    const LINE_WIDTH = 300;
+    const LINE_WIDTH = svg_width * 0.75;
 
     function dragged(event, datum) {
         var new_x = event.x;
@@ -169,5 +216,42 @@ function prepare_event_period_selection() {
 
     spiral_chart_svg.selectAll("circle")
         .call(d3.drag()
-            .on("drag", dragged));
+            .on("drag", dragged)
+            .on("end", updateSpiralChart));
+}
+
+function updateSpiralChart() {
+    var svg_width = parseInt(spiral_chart_svg.style("width").slice(0, -2));
+    var svg_height = parseInt(spiral_chart_svg.style("height").slice(0, -2));
+    const margin = { "top": 0, "bottom": 20, "left": 20, "right": 20 }
+    const chartRadius = svg_height / 2 - margin.bottom;
+
+    dataset_spiral_chart = treatDataset();
+    var domain = d3.extent(dataset_spiral_chart, datum => datum["Models"]);
+    spiral_color_scale.domain(domain);
+
+    spiral_chart_svg.select("#bottom_spiral_scale").text(domain[0]);
+    spiral_chart_svg.select("#top_spiral_scale").text(domain[1]);
+
+    spiral_chart_svg.select("g#spiral_group").remove();
+
+    spiral_heatmap = spiralHeatmap()
+        .radius(chartRadius)
+        .holeRadiusProportion(0)
+        .arcsPerCoil(selected_period_months['div'])
+        .coilPadding(0.05)
+        .arcLabel("Month")
+        .coilLabel("Year")
+
+    const g = spiral_chart_svg.append("g")
+        .attr("id", "spiral_group")
+        .attr("transform", "translate(" + 
+            (svg_width / 2) + "," + 
+            (svg_height / 2 - margin.bottom + margin.top) + ")");
+
+    g.datum(dataset_spiral_chart)
+        .call(spiral_heatmap);
+
+    g.selectAll(".arc").selectAll("path")
+        .style("fill", function (d) { return spiral_color_scale(d["Models"]); })
 }
